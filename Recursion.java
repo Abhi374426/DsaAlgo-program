31aa09427e86f5b8b3a8d04957d4decbf9d60cf5
Abhi374426 <115145261+Abhi374426@users.noreply.github.com>
2024-04-17 11:37:23 +0530

    recursion

---
+++ Recursion.java
@@ -0,0 +1,73 @@
+public class Recursion {
+    public static void main(String[] args) {
+        inner s1=new inner();
+        // s1.printN(10);
+        // s1.printInRevers(3);
+        // s1.printeven(10);
+        // s1.printevenrevers(10);
+        // s1.printoddnum(10);
+        s1.printoddnumrev(10);
+    }
+   
+}
+class inner{
+    public void printN(int n){
+        if (n>0) {
+            printN(n-1);
+            System.out.println(n);
+        }
+    }
+public void printInRevers(int n)
+{
+    if (n>0) {
+        System.out.println(n);
+        printInRevers(n-1);
+    }
+}
+public void printeven(int n){
+    if (n>0) {
+        printeven(n-1);
+        if (n%2==0) {
+            System.out.println(n);
+
+        }
+      
+    }
+}
+public void printevenrevers(int n){
+    if (n>0) {
+       
+        if (n%2==0) {
+            System.out.println(n);
+
+        }
+        printevenrevers(n-1);
+      
+    }
+}
+public void printoddnum(int n){
+    if (n>0) {
+       printoddnum(n-1);
+        if (n%2!=0) {
+            System.out.println(n);
+
+        }
+        
+      
+    }
+}
+public void printoddnumrev(int n){
+    if (n>0) {
+      
+        if (n%2!=0) {
+            System.out.println(n);
+
+        }
+        printoddnumrev(n-1);
+
+        
+      
+    }
+}
+
+}
